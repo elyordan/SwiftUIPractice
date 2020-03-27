b4d2e9ff0f38be974421bee46649d9488c65390f
elyordan <elyordan_21@hotmail.com>
2020-03-27 01:04:58 -0400

    add second apple tutorial

---
+++ BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/Supporting Views/LandmarkRow.swift
@@ -0,0 +1,45 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Yunior Lopez on 3/26/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    
+    var landmark:Landmark
+    
+    var body: some View {
+        
+        HStack {
+            
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            
+            Text(landmark.name)
+            
+            Spacer()
+            
+        }.padding(.leading, 10)
+        
+    }
+    
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        Group {
+            
+            LandmarkRow(landmark: landmarkData[0])
+            
+            LandmarkRow(landmark: landmarkData[1])
+            
+        }.previewLayout(.fixed(width: 300, height: 70))
+        
+    }
+}
